a646dfb21a46fa6e18665619fda9607772ceeaaa
BenjaminF <benji-fo@outlook.fr>
2024-09-23 21:19:37 +0200

    add navigation and start style

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Sidebar.scss'
+import logo from '../../assets/images/basic_fat_logo.svg'
 
 export default function Sidebar() {
     return (
         <>
-            <img src="src/assets/images/basic_fat_logo.svg" alt="Logo Basic Fat" />
+            <div className='sidebar__container'>
+                <img className='sidebar__logo' src={logo} alt="Logo Basic Fat" />
+                <nav className='sidebar__nav'>
+                    <ul>
+                        <li className='sidebar__nav__element'><a href="/">Home</a></li>
+                        <li className='sidebar__nav__element'><a href="/dashboard">Dashboard</a></li>
+                        <li className='sidebar__nav__element'><a href="/training">Training</a></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
