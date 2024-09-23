827b7b8ed4c8e457f21b5081226e7b1ac48d16d7
Samuela <115798007+koffi-samuela@users.noreply.github.com>
2024-09-23 23:46:39 +0200

    authentification ( enregistrement et connexion )

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { useAuth } from '../../utils/AuthProvider';
+import { Router } from 'react-router-dom';
+
+export default function Login() {
+    const { login } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const success = login(email, password);
+    if (success) {
+      alert('Connexion réussie !');
+    //   Router()
+    } else {
+      alert('Échec de la connexion. Vérifiez vos informations.');
+      setPassword('')
+      setEmail('')
+    }
+  };
+  return (
+    <section id='login'>
+             <form onSubmit={handleLogin}>
+      <div>
+        <label>Email:</label>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required 
+        />
+      </div>
+      <div>
+        <label>Mot de passe:</label>
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
+        />
+      </div>
+      <button type="submit">Se connecter</button>
+    </form>
+    </section>
+  )
+}
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useAuth } from '../../utils/AuthProvider';
 
 export default function Register() {
+  const { register } = useAuth();
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [subscription, setSubscription] = useState('9.99');  // Choix par défaut
+
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(email, username, password, subscription);
+    
+    register(email, username, password, subscription);
+    alert('Utilisateur enregistré avec succès !');
+  };
+  // const { authToken, logout, users } = useAuth();
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!authToken) {
+  //     navigate('/register'); // Redirige vers la page de connexion si non authentifié
+  //   }
+  // }, [authToken, navigate]);
+  
+  
+
+
   return (
-    <div>Register</div>
+    <section id='register'>
+        <form onSubmit={handleRegister}>
+      <div>
+        <label>Email:</label>
+        <input 
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required 
+        />
+      </div>
+      <div>
+        <label>Nom d'utilisateur:</label>
+        <input 
+          type="text" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+          required 
+        />
+      </div>
+      <div>
+        <label>Mot de passe:</label>
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
+          current-password = 'true'
+        />
+      </div>
+      <div>
+        <label>Type d'abonnement:</label>
+        <select value={subscription} onChange={(e) => setSubscription(e.target.value)}>
+          <option value="9.99">9,99€</option>
+          <option value="19.99">19,99€</option>
+          <option value="29.99">29,99€</option>
+        </select>
+      </div>
+      <button type="submit">S'inscrire</button>
+    </form>
+    </section>
   )
 }
