34b326d7da6ce136794be05f8e0a87b46bb49b51
BenjaminF <benji-fo@outlook.fr>
2024-09-27 15:26:46 +0200

    change header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <>
             <div className='header-container'>
-                <h1>BASIC FAT</h1>
+                <h1 className='title'>BASIC FAT</h1>
             </div>
         </>
     )
